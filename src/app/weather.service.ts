c0003f2c665bea90f22ec5c1d5936470eca9b05d
beforeadream <beforeadream@gmail.com>
2017-07-02 18:04:48 +0300

    Build app for gh-pages

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -11,22 +11,28 @@ export class WeatherService {
   }
 
   getCurrentWeatherByCity(city) {
+    const https_hack = 'https://cors-anywhere.herokuapp.com/';
+
     return this.http
-      .get(`//api.openweathermap.org/data/2.5/weather?q=${city}&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric`)
+      .get(`${https_hack}http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric`)
       .map((res) => res.json())
       .catch((error: any) => Observable.throw(error.json().error || 'Hey! Something went wrong'));
   }
 
   getDailyForecastByCity(city) {
+    const https_hack = 'https://cors-anywhere.herokuapp.com/';
+
     return this.http
-      .get(`//api.openweathermap.org/data/2.5/forecast/daily?q=${city}&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric`)
+      .get(`${https_hack}http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric`)
       .map((res) => res.json())
       .catch((error: any) => Observable.throw(error.json().error || 'Hey! Something went wrong'));
   }
 
   getHourlyForecastByCity(city) {
+    const https_hack = 'https://cors-anywhere.herokuapp.com/';
+
     return this.http
-      .get(`//api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric`)
+      .get(`${https_hack}http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric`)
       .map((res) => res.json())
       .catch((error: any) => Observable.throw(error.json().error || 'Hey! Something went wrong'));
   }
