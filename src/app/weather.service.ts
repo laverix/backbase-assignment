adc983c010cedaf2fdb6ec128c36abe6a46fe416
beforeadream <beforeadream@gmail.com>
2017-07-02 17:27:54 +0300

    Added service for managing calls on openweather api and its spec file

---
+++ src/app/weather.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class WeatherService {
+
+  constructor(private http: Http) { }
+
+  getCurrentWeatherByCity(city) {
+    return this.http
+      .get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric`)
+      .map((res) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Hey! Something went wrong'));
+  }
+
+  getDailyForecastByCity(city) {
+    return this.http
+      .get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric`)
+      .map((res) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Hey! Something went wrong'));
+  }
+
+  getHourlyForecastByCity(city) {
+    return this.http
+      .get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric`)
+      .map((res) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Hey! Something went wrong'));
+  }
+}
