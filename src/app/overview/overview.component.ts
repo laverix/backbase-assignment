ae2a6d276ce02d02fbee502fe006333846c959e6
beforeadream <beforeadream@gmail.com>
2017-07-02 17:31:05 +0300

    Added overview component

---
+++ src/app/overview/overview.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-overview',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.css']
+})
+export class OverviewComponent implements OnInit {
+  @Input() cities;
+
+  constructor() { }
+
+  ngOnInit() {  }
+}
