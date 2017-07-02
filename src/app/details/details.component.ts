8c017efa80ac18dc893c97efde5738738d932808
beforeadream <beforeadream@gmail.com>
2017-07-02 17:31:30 +0300

    Added details component

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {WeatherService} from '../weather.service';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit {
+  id: string;
+  weather;
+
+  constructor(private route: ActivatedRoute, private weatherService: WeatherService, private router: Router) { }
+
+  private getHourlyForecastByCity(id) {
+    this.weatherService.getHourlyForecastByCity(id).subscribe(response => this.weather = response);
+  }
+
+  public goBack() {
+    this.router.navigate(['weather']);
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+
+      this.getHourlyForecastByCity(this.id);
+    });
+  }
+}
