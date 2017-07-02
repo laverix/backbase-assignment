2504d4497385b1740a184d8fa3aaad34c3961c13
beforeadream <beforeadream@gmail.com>
2017-07-02 17:50:55 +0300

    Changed urls to //

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -12,21 +12,21 @@ export class WeatherService {
 
   getCurrentWeatherByCity(city) {
     return this.http
-      .get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric`)
+      .get(`//api.openweathermap.org/data/2.5/weather?q=${city}&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric`)
       .map((res) => res.json())
       .catch((error: any) => Observable.throw(error.json().error || 'Hey! Something went wrong'));
   }
 
   getDailyForecastByCity(city) {
     return this.http
-      .get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric`)
+      .get(`//api.openweathermap.org/data/2.5/forecast/daily?q=${city}&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric`)
       .map((res) => res.json())
       .catch((error: any) => Observable.throw(error.json().error || 'Hey! Something went wrong'));
   }
 
   getHourlyForecastByCity(city) {
     return this.http
-      .get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric`)
+      .get(`//api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric`)
       .map((res) => res.json())
       .catch((error: any) => Observable.throw(error.json().error || 'Hey! Something went wrong'));
   }
