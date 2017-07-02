a7fdc163add7b635c56889b8c8db30384e1149f4
beforeadream <beforeadream@gmail.com>
2017-07-02 17:30:28 +0300

    Added cool greating component ❤️

---
+++ src/app/greetings/greetings.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-greetings',
+  templateUrl: './greetings.component.html',
+  styleUrls: ['./greetings.component.css']
+})
+export class GreetingsComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  private redirectToWeatherComponent() {
+    this.router.navigate(['weather']);
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.redirectToWeatherComponent();
+    }, 1500)
+  }
+}
